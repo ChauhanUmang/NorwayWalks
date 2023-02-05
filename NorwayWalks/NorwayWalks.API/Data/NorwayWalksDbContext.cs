52da82409834dc97c205540cf0189590195a095d
Umang Chauhan <umang4191@outlook.com>
2023-02-05 20:19:40 +0200

    Created Models and applied migrations

---
+++ NorwayWalks/NorwayWalks.API/Data/NorwayWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorwayWalks.API.Models.Domain;
+
+namespace NorwayWalks.API.Data
+{
+    public class NorwayWalksDbContext : DbContext
+    {
+        public NorwayWalksDbContext(DbContextOptions<NorwayWalksDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
