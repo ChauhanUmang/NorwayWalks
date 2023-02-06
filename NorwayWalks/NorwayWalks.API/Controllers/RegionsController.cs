fbe83c6e6147f7f91d4932badf33d7e781283ecf
Umang Chauhan <umang4191@outlook.com>
2023-02-06 15:04:23 +0200

    Completed CRUD for Region

--- NorwayWalks/NorwayWalks.API/Controllers/RegionsController.cs
+++ NorwayWalks/NorwayWalks.API/Controllers/RegionsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using NorwayWalks.API.Models.Domain;
+using NorwayWalks.API.Models.DTO;
 using NorwayWalks.API.Repositories;
 
 namespace NorwayWalks.API.Controllers
@@ -19,7 +20,7 @@ namespace NorwayWalks.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllRegions() 
+        public async Task<IActionResult> GetAllRegionsAsync() 
         {
             var regions = await _regionRepository.GetAllAsync();
 
@@ -43,6 +44,93 @@ namespace NorwayWalks.API.Controllers
 
             var regionsDTO = _mapper.Map<List<Models.DTO.Region>>(regions);
             return Ok(regionsDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetRegionAsync")]
+        public async Task<IActionResult> GetRegionAsync(Guid id)
+        {
+            var region = await _regionRepository.GetAsync(id);
+
+            if(region is null)
+            {
+                return NotFound();
+            }
+
+            var regionDTO = _mapper.Map<Models.DTO.Region>(region);
+            return Ok(regionDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRegionAsync(AddRegionRequest addRegionRequest)
+        {
+            //Request(DTO) to domain model
+            var region = new Models.Domain.Region()
+            {
+                Code = addRegionRequest.Code,
+                Name = addRegionRequest.Name,
+                Area = addRegionRequest.Area,
+                Lat = addRegionRequest.Lat,
+                Long = addRegionRequest.Long,
+                Population = addRegionRequest.Population
+            };
+
+            //pass details to repository
+            var response = await _regionRepository.AddAsync(region);
+
+            //Convert back to DTO
+            _mapper.Map<Models.DTO.Region>(response);
+            return CreatedAtAction(nameof(GetRegionAsync), new {id = response.Id}, response);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteRegionAsync(Guid id)
+        {
+            //Get the region from the database
+            var region = await _regionRepository.DeleteAsync(id);
+
+            //if null, send notfound
+            if (region is null)
+            {
+                return NotFound();
+            }
+
+            //if region not null, delete it and convert response to DTO model
+            var regionDTO = _mapper.Map<Models.DTO.Region>(region);
+            //return Ok response
+            return Ok(regionDTO);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateRegionAsync([FromRoute]Guid id, [FromBody]UpdateRegionRequest updateRegionRequest)
+        {
+            //Convert DTO to domain model
+            var region = new Models.Domain.Region()
+            {
+                Code = updateRegionRequest.Code,
+                Name = updateRegionRequest.Name,
+                Area = updateRegionRequest.Area,
+                Lat = updateRegionRequest.Lat,
+                Long = updateRegionRequest.Long,
+                Population = updateRegionRequest.Population
+            };
+
+            //Update region using repository
+            var updatedregion = await _regionRepository.UpdateAsync(id,region);
+
+            //if null return not found
+            if(updatedregion is null)
+            {
+                return NotFound();
+            }
+            //Convert domain back to DTO
+            var updateRegionDTO = _mapper.Map<Models.DTO.Region>(updatedregion);
+
+            //Return ok response
+            return Ok(updateRegionDTO);
         }
     }
 }
--- NorwayWalks/NorwayWalks.API/Repositories/RegionRepository.cs
+++ NorwayWalks/NorwayWalks.API/Repositories/RegionRepository.cs
@@ -13,9 +13,56 @@ namespace NorwayWalks.API.Repositories
             _db = db;
         }
 
+        public async Task<Region> AddAsync(Region region)
+        {
+            region.Id = Guid.NewGuid();
+            await _db.AddAsync(region);
+            await _db.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> DeleteAsync(Guid id)
+        {
+            var region = await _db.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if(region is null)
+            {
+                return null;
+            }
+
+            //Delete the region
+            _db.Regions.Remove(region);
+            await _db.SaveChangesAsync();
+            return region;
+        }
+
         public async Task<IEnumerable<Region>> GetAllAsync()
         {
             return await _db.Regions.ToListAsync();
+        }
+
+        public async Task<Region> GetAsync(Guid id)
+        {
+            return await _db.Regions.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Region> UpdateAsync(Guid id, Region region)
+        {
+            var existingregion = await _db.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if(existingregion is null)
+            {
+                return null;
+            }
+
+            existingregion.Code = region.Code;
+            existingregion.Name = region.Name;
+            existingregion.Area = region.Area;
+            existingregion.Lat = region.Lat;
+            existingregion.Long = region.Long;
+            existingregion.Population = region.Population;
+
+            //_db.Regions.Update(region);
+            await _db.SaveChangesAsync();
+            return existingregion;
         }
     }
 }
